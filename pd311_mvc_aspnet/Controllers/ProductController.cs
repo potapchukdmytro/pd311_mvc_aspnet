4435fecb1c9104f671b9c2608d5e0bacde4ced74
potapchukdmytro <traiget@gmail.com>
2025-02-10 17:24:24 +0200

    Add project files.

---
+++ pd311_mvc_aspnet/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pd311_mvc_aspnet.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ pd311_mvc_aspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pd311_mvc_aspnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
